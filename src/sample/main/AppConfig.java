383b33bd98a025fe56907f7eae292fca5d9993db
Sachithra Dilshan <itismesachithra@gmail.com>
2019-08-13 23:53:21 +0530

    point of sale

---
+++ src/sample/main/AppConfig.java
@@ -0,0 +1,7 @@
+package sample.main;
+
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class AppConfig {
+}
---
+++ src/sample/main/Main.java
@@ -0,0 +1,29 @@
+package sample.main;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+import java.lang.annotation.Annotation;
+
+public class Main extends Application {
+
+    @Override
+    public void start(Stage primaryStage) throws Exception{
+        Parent root = FXMLLoader.load(getClass().getResource("../view/sample.fxml"));
+        primaryStage.setTitle("Hello World");
+        primaryStage.setScene(new Scene(root, 400, 400));
+        primaryStage.show();
+    }
+
+
+    public static void main(String[] args) {
+        launch(args);
+        AnnotationConfigApplicationContext ctx=new AnnotationConfigApplicationContext();
+        ctx.register(AppConfig.class);
+        ctx.refresh();
+    }
+}
